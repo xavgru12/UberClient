853aa6d1667b4c2727f886740b73d7a66c2d51f8
Xaver Gruber <xaver.max.gruber@gmail.com>
2023-09-11 17:52:04 +0200

    remove all comments without http:// or hex comments, used this regex: (?<!:)//\s(?!0).*\n

--- UberStrike.UnitySdk/Cmune/Realtime/Common/PerfCounter.cs
+++ UberStrike.UnitySdk/Cmune/Realtime/Common/PerfCounter.cs
@@ -1,9 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Cmune.Realtime.Common.PerfCounter
-// Assembly: UberStrike.UnitySdk, Version=1.0.1.0, Culture=neutral, PublicKeyToken=null
-// MVID: 5841A2D1-61BC-4235-BEF7-EC54B624B7CE
-// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\UberStrike4-3-9\UberStrike_Data\Managed\UberStrike.UnitySdk.dll
-
+﻿
 namespace Cmune.Realtime.Common
 {
   public enum PerfCounter
--- UberStrike.UnitySdk/UberStrike/Realtime/Client/PhotonPeerListener.cs
+++ UberStrike.UnitySdk/UberStrike/Realtime/Client/PhotonPeerListener.cs
@@ -1,9 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Uberstrike.Realtime.Client.PhotonPeerListener
-// Assembly: UberStrike.UnitySdk, Version=1.0.1.0, Culture=neutral, PublicKeyToken=null
-// MVID: 5841A2D1-61BC-4235-BEF7-EC54B624B7CE
-// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\UberStrike4-3-9\UberStrike_Data\Managed\UberStrike.UnitySdk.dll
-
+﻿
 using ExitGames.Client.Photon;
 using System;
 using System.Runtime.Serialization;
