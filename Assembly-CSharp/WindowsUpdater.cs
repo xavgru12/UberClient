efdf33c5ba0d78dca302546fed82566c4c09b1ae
Xaver Gruber <xaver.max.gruber@gmail.com>
2026-08-06 14:06:18 -0700

    remove new gui spawn

--- Assembly-CSharp/WindowsUpdater.cs
+++ Assembly-CSharp/WindowsUpdater.cs
@@ -198,6 +198,7 @@ internal class WindowsUpdater
 		DeleteUnnecessary();
 		AuthenticationManager._progress.Text = "Installing update...";
 		CopyFiles(Path.Combine(gamepath, "Updates\\UberStrike"), gamepath);
+<<<<<<< Updated upstream
 		try
 		{
 			Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "UberStrike.exe")).WaitForExit();
@@ -207,6 +208,8 @@ internal class WindowsUpdater
 			UnityEngine.Debug.LogError(e);
 		}
 		Application.Quit();
+=======
+>>>>>>> Stashed changes
 	}
 
 	private static void CopyFiles(string sourcePath, string destinationPath)
